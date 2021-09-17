8a893da88b757cdbcec516e96b8866aca1af8264
ashwani1218 <ashwanipandey1218@gmail.com>
2021-09-17 16:09:52 +0530

    Added search for Members, closes #3

--- src/main/java/com/ashwani/family/business/controller/MemberController.java
+++ src/main/java/com/ashwani/family/business/controller/MemberController.java
@@ -3,12 +3,11 @@ package com.ashwani.family.business.controller;
 import com.ashwani.family.business.service.MemberService;
 import com.ashwani.family.infra.model.request.AddMemberRequest;
 import com.ashwani.family.infra.model.response.AddMemberResponse;
+import com.ashwani.family.infra.model.response.FindAllMembersResponse;
+import com.ashwani.family.infra.model.response.FindMemberResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -22,5 +21,17 @@ public class MemberController {
     public AddMemberResponse addMember(@RequestBody AddMemberRequest request){
         log.info("Add Family member request [{}]", request);
         return memberService.addMember(request);
+    }
+
+    @GetMapping("/members")
+    public FindAllMembersResponse findAllMembers(){
+        log.info("Find All family members request");
+        return memberService.findAllMembers();
+    }
+
+    @GetMapping("/member/{id}")
+    public FindMemberResponse findMember(@PathVariable Long id){
+        log.info("Find Family member with id [{}]",id);
+        return memberService.findMemberById(id);
     }
 }
