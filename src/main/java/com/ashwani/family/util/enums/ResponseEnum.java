2a35fca315b53deba4bbf118d42485dd95511990
ashwani1218 <ashwanipandey1218@gmail.com>
2021-10-04 23:59:02 +0530

    Update ResponseEnum.java

--- src/main/java/com/ashwani/family/util/enums/ResponseEnum.java
+++ src/main/java/com/ashwani/family/util/enums/ResponseEnum.java
@@ -2,7 +2,14 @@ package com.ashwani.family.util.enums;
 
 public enum ResponseEnum {
 
-    SUCCESS (200, "Success");
+    SUCCESS (200, "Success"),
+    ADD_MEMBER_SUCCESS(200,"Member Added Successfully"),
+    ADD_MEMBER_FAILURE(409,"Unable to add Member"),
+    ADD_DOCUMENT_SUCCESS(200, "Document Added Successfully"),
+    ADD_DOCUMENT_FAILURE(409,"Unable to add Document"),
+    GET_DOC_TYPE_SUCCESS(200,"Doc types fetched successfully"),
+    ADD_DOC_TYPE_SUCCESS(200,"Document Type added successfully"),
+    ADD_DOCUMENT_TYPE_FAILURE(409,"Unable to add Document type");
 
     private final int code;
     private final String message;
